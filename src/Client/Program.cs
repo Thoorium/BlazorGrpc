b05d3d9892481f1098ad60e50dee2e6b1886db22
Olivier Beaudoin <contact@olivierbeaudoin.ca>
2020-04-04 11:57:25 -0400

    Add project files.

---
+++ src/Client/Program.cs
@@ -0,0 +1,36 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http;
+using Grpc.Net.Client.Web;
+using Microsoft.AspNetCore.Components;
+using Grpc.Net.Client;
+using GrpcGreeter;
+
+namespace BlazorGrpc.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddBaseAddressHttpClient();
+
+            builder.Services.AddSingleton(services =>
+            {
+                // Create a gRPC-Web channel pointing to the backend server
+                var httpClient = new HttpClient(new GrpcWebHandler(GrpcWebMode.GrpcWeb, new HttpClientHandler()));
+                var baseUri = services.GetRequiredService<NavigationManager>().BaseUri;
+                var channel = GrpcChannel.ForAddress(baseUri, new GrpcChannelOptions { HttpClient = httpClient });
+
+                // Now we can instantiate gRPC clients for this channel
+                return new Greeter.GreeterClient(channel);
+            });
+
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
---
+++ src/Server/Services/GreeterService.cs
@@ -0,0 +1,15 @@
+﻿using Grpc.Core;
+using GrpcGreeter;
+using System.Threading.Tasks;
+
+namespace BlazorGrpc.Server.Services
+{
+    public class GreeterService : Greeter.GreeterBase
+    {
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            var reply = new HelloReply { Message = string.IsNullOrWhiteSpace(request.Name) ? "Please give me your name." : $"Hello from gRPC, {request.Name}!" };
+            return Task.FromResult(reply);
+        }
+    }
+}
